eec7c9d79c9cd4b04ea76156086c022921a236ba
Titouan <titouan.lacombe99@gmail.com>
2022-11-18 14:11:26 +0100

    fixes

--- Dockerfile
+++ Dockerfile
@@ -20,5 +20,5 @@ RUN useradd -m -s /bin/bash -G sudo user
 RUN echo "user ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 USER user
 
-COPY setup.sh /bin/setup.sh
-RUN sudo -E setup.sh
+COPY setup.sh setup.sh
+RUN ./setup.sh
--- setup.sh
+++ setup.sh
@@ -1,47 +1,57 @@
 #!/bin/sh
 # Script to setup a PC workstation
 # Run as root (with a root shell or "sudo -E setup.sh" with setup.sh in PATH)
+# Example: sudo cp setup.sh /bin/workstation-setup && sudo workstation-setup
 set -xe # Print commands and exit on error
 
-# --- Variables (edit manually when on distro) ---
-# PM_UPDATE="apt update"
-# PM_UPGRADE="apt upgrade -y"
-# PM_INSTALL="apt install -y"
+# --- Variables (edit manually) ---
+PM_UPDATE="apt update"
+PM_UPGRADE="apt upgrade -y"
+PM_INSTALL="apt install -y"
+GIT_USERNAME="Titouan Lacombe"
+GIT_EMAIL="titouan.lacombe99@gmail.com"
 
 # --- Installs ---
 # Apt packages
-$PM_UPDATE
-$PM_UPGRADE
-$PM_INSTALL curl git zsh flatpak clang fonts-firacode python3-pip
+sudo $PM_UPDATE
+sudo $PM_UPGRADE
+sudo $PM_INSTALL curl git zsh flatpak clang fonts-firacode python3-pip docker-compose
+
+# VSCode
+if ! command -v code; then
+	curl -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" > vscode.deb
+	sudo $PM_INSTALL ./vscode.deb
+	rm vscode.deb
+fi
 
 # Flatpak packages
 flatpak remote-add --if-not-exists flathub "https://flathub.org/repo/flathub.flatpakrepo"
 flatpak install -y com.brave.Browser com.discordapp.Discord com.valvesoftware.Steam
 
 # Zsh theme
-git clone --depth=1 "https://github.com/romkatv/powerlevel10k.git" ~/powerlevel10k""
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+if [ ! -d ~/powerlevel10k ]; then
+	git clone --depth=1 "https://github.com/romkatv/powerlevel10k.git" ~/powerlevel10k
+	echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+fi
 
 # Makepie
 pip3 install makepie
 
-# VS Code
-curl "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" > vscode.deb
-$PM_INSTALL ./vscode.deb
-rm vscode.deb
-
 # Docker
-curl -fsSL "https://get.docker.com" | sh
-$PM_INSTALL docker-compose
-usermod -aG docker $USER
 # TODO rootless docker?
+if ! command -v docker; then
+	curl -fsSL "https://get.docker.com" | sh
+	usermod -aG docker $USER
+fi
 
 # Rust
-curl --proto '=https' --tlsv1.3 "https://sh.rustup.rs" -sSf | sh
+if ! command -v rustup; then
+	curl --proto '=https' --tlsv1.3 "https://sh.rustup.rs" -sSf | sh -s -- -y
+fi
 
 # --- Configs ---
 # Git config
-git config --global user.name "Titouan Lacombe" && git config --global user.email "titouan.lacombe99@gmail.com"
+git config --global user.name $GIT_USERNAME && git config --global user.email $GIT_EMAIL
 
 # --- End ---
 echo "Setup complete !"
