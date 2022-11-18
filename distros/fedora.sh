ded06e03cfec530ca4cd1722fc1310f582c3db98
Titouan <titouan.lacombe99@gmail.com>
2022-11-18 15:27:23 +0100

    added packages

--- distros/fedora.sh
+++ distros/fedora.sh
@@ -1,4 +1,4 @@
 PM_UPDATE="dnf update -y"
 PM_UPGRADE="dnf upgrade -y"
 PM_INSTALL="dnf install -y"
-PACKAGES="curl git zsh flatpak clang fonts-firacode python3-pip docker-compose"
+PACKAGES="curl git zsh vlc ffmpeg flatpak clang fonts-firacode python3-pip docker-compose"
