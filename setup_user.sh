3fb01ac1c2e9505cb011cdd2b6903051abab9640
Titouan <titouan.lacombe99@gmail.com>
2022-11-18 23:18:03 +0100

    fix sudo

--- setup_user.sh
+++ setup_user.sh
@@ -4,8 +4,8 @@ set -xe
 # Install sudo if not already installed
 if ! command -v sudo; then $PM_UPDATE && $PM_INSTALL sudo; fi
 
-# Create user and add to sudoers
-useradd -ms /bin/bash -G sudo fakeuser
+# Create user
+useradd -ms /bin/bash fakeuser
 echo "fakeuser ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
 chown -R fakeuser:fakeuser /home/fakeuser
